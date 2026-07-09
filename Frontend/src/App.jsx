0b8dff44be86819836c33d30eef21da4fac638c1
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-09 16:29:50 +0500

    Image Submit Form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import CreatPost from './pages/CreatPost.jsx'
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<h1>Hello world</h1>}/>
+        <Route path='/create-post' element={<CreatPost />}/>
         <Route path='/about' element={<h1>About us</h1>}/>
       
       </Routes>
---
+++ Frontend/src/pages/CreatPost.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CreatPost = () => {
+  return (
+    <section className='create-post-section'>
+        <h1>Create post Vamos</h1>
+
+        <form >
+            <input type="file" name="image" accept='image/*' />
+            <input type="text" name='caption' required/>
+            <button type='submit'>Jaan Deyo</button>
+        </form>
+    </section>
+  )
+}
+
+export default CreatPost
