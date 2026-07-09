39423bd87c304bac61f40df7fef798a546ed6ad4
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-09 17:34:47 +0500

    Frontend Complete

--- Frontend/src/pages/CreatPost.jsx
+++ Frontend/src/pages/CreatPost.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreatPost = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        axios.post("http://localhost:3000/create-post", formData)
+        .then((response) => {
+            console.log(response.data);
+            navigate('/feed');
+            e.target.reset();
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Error creating post");
+        })
+    }
+
   return (
     <section className='create-post-section'>
         <h1>Create post Vamos</h1>
 
-        <form >
+        <form onSubmit={handleSubmit}>
             <input type="file" name="image" accept='image/*' />
             <input type="text" name='caption' placeholder='Enter caption' required/>
             <button type='submit'>Jaan Deyo</button>
